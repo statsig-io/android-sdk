446b013e8cc7875b9d5eb126b60715b2238082b6
daniel-statsig <daniel@statsig.com>
2022-02-04 17:12:28 -0800

    fixed test

--- src/test/java/com/statsig/androidsdk/StoreTest.kt
+++ src/test/java/com/statsig/androidsdk/StoreTest.kt
@@ -1,74 +1,74 @@
 package com.statsig.androidsdk
 
-import android.content.Context
 import io.mockk.every
 import io.mockk.mockkObject
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 
-
 class StoreTest {
     @Before
     internal fun setup() {
 
         mockkObject(StatsigUtil)
-        every {
-            StatsigUtil.getHashedString(any())
-        } answers {
-            firstArg<String>() + "!"
-        }
-
+        every { StatsigUtil.getHashedString(any()) } answers { firstArg<String>() + "!" }
     }
 
-    private fun getInitValue(value: String, inExperiment: Boolean = false, active: Boolean = false): InitializeResponse {
+    private fun getInitValue(
+            value: String,
+            inExperiment: Boolean = false,
+            active: Boolean = false
+    ): InitializeResponse {
         return InitializeResponse(
-            featureGates = mapOf(
-                "gate!" to APIFeatureGate("gate", inExperiment, "id", arrayOf())
-            ),
-            configs = mapOf(
-                "config!" to APIDynamicConfig(
-                    "config!",
-                    mutableMapOf("key" to value),
-                    "id",
-                    arrayOf(),
-                ),
-                "exp!" to APIDynamicConfig(
-                    "exp!",
-                    mutableMapOf("key" to value),
-                    "id",
-                    arrayOf(),
-                    isDeviceBased = false,
-                    isUserInExperiment = inExperiment,
-                    isExperimentActive = active,
-                ),
-                "device_exp!" to APIDynamicConfig(
-                    "device_exp!",
-                    mutableMapOf("key" to value),
-                    "id",
-                    arrayOf(),
-                    isDeviceBased = true,
-                    isUserInExperiment = inExperiment,
-                    isExperimentActive = active,
-                ),
-                "exp_non_stick!" to APIDynamicConfig(
-                    "non_stick_exp!",
-                    mutableMapOf("key" to value),
-                    "id",
-                    arrayOf(),
-                    isDeviceBased = false,
-                    isUserInExperiment = inExperiment,
-                    isExperimentActive = active,
-                ),
-            ),
-            hasUpdates = false,
-            time = 1621637839,
+                featureGates =
+                        mapOf("gate!" to APIFeatureGate("gate", inExperiment, "id", arrayOf())),
+                configs =
+                        mapOf(
+                                "config!" to
+                                        APIDynamicConfig(
+                                                "config!",
+                                                mutableMapOf("key" to value),
+                                                "id",
+                                                arrayOf(),
+                                        ),
+                                "exp!" to
+                                        APIDynamicConfig(
+                                                "exp!",
+                                                mutableMapOf("key" to value),
+                                                "id",
+                                                arrayOf(),
+                                                isDeviceBased = false,
+                                                isUserInExperiment = inExperiment,
+                                                isExperimentActive = active,
+                                        ),
+                                "device_exp!" to
+                                        APIDynamicConfig(
+                                                "device_exp!",
+                                                mutableMapOf("key" to value),
+                                                "id",
+                                                arrayOf(),
+                                                isDeviceBased = true,
+                                                isUserInExperiment = inExperiment,
+                                                isExperimentActive = active,
+                                        ),
+                                "exp_non_stick!" to
+                                        APIDynamicConfig(
+                                                "non_stick_exp!",
+                                                mutableMapOf("key" to value),
+                                                "id",
+                                                arrayOf(),
+                                                isDeviceBased = false,
+                                                isUserInExperiment = inExperiment,
+                                                isExperimentActive = active,
+                                        ),
+                        ),
+                hasUpdates = false,
+                time = 1621637839,
         )
     }
 
     @Test
     fun testCacheById() {
-        assertTrue("Should Fail", false)
         val store = Store("jkw", null, TestSharedPreferences())
         store.save(getInitValue("v0", inExperiment = true, active = true))
         store.loadAndResetForUser("dloomb", null)
@@ -85,16 +85,17 @@ class StoreTest {
     fun testStickyUserExperimentMigration() {
         val sharedPrefs = TestSharedPreferences()
         StatsigUtil.saveStringToSharedPrefs(
-            sharedPrefs,
-            "Statsig.STICKY_USER_EXPERIMENTS",
-            "{user_id: \"dloomb\", values: {\"bar!\": {\"value\": {\"foo\": \"aValue\"}}}}"
+                sharedPrefs,
+                "Statsig.STICKY_USER_EXPERIMENTS",
+                "{user_id: \"dloomb\", values: {\"bar!\": {\"value\": {\"foo\": \"aValue\"}}}}"
         )
 
         val store = Store("dloomb", null, sharedPrefs)
         val exp = store.getExperiment("bar", true)
         assertEquals("aValue", exp.getValue()["foo"])
 
-        val savedVal = StatsigUtil.getFromSharedPrefs(sharedPrefs, "Statsig.STICKY_USER_EXPERIMENTS")
+        val savedVal =
+                StatsigUtil.getFromSharedPrefs(sharedPrefs, "Statsig.STICKY_USER_EXPERIMENTS")
         assertNull(savedVal)
     }
 
@@ -111,7 +112,7 @@ class StoreTest {
 
     @Test
     fun testStickyBucketing() {
-        val store = Store("jkw",null,  TestSharedPreferences())
+        val store = Store("jkw", null, TestSharedPreferences())
         store.save(getInitValue("v0", inExperiment = true, active = true))
 
         // getting values with keepDeviceValue = false first
@@ -148,7 +149,8 @@ class StoreTest {
         assertEquals("v1", deviceExp.getString("key", ""))
         assertEquals("v2", nonStickExp.getString("key", ""))
 
-        // update the experiments so that the user is no longer in experiments, should still be sticky for the right ones
+        // update the experiments so that the user is no longer in experiments, should still be
+        // sticky for the right ones
         store.save(getInitValue("v3", inExperiment = false, active = true))
 
         config = store.getExperiment("config", true)
@@ -175,7 +177,7 @@ class StoreTest {
 
     @Test
     fun testStickyBehaviorWhenResettingUser() {
-        val store = Store("jkw",null, TestSharedPreferences())
+        val store = Store("jkw", null, TestSharedPreferences())
         store.save(getInitValue("v0", inExperiment = true, active = true))
 
         // getting values with keepDeviceValue = false first
@@ -229,7 +231,8 @@ class StoreTest {
         assertEquals("v0", deviceExp.getString("key", ""))
         assertEquals("v0", nonStickExp.getString("key", ""))
 
-        // Re-create store with a different user ID, update the values, user should still get sticky value for device and only device
+        // Re-create store with a different user ID, update the values, user should still get sticky
+        // value for device and only device
         store = Store("tore", null, sharedPrefs)
         store.save(getInitValue("v1", inExperiment = true, active = true))
 
@@ -254,6 +257,5 @@ class StoreTest {
         assertEquals("v0", exp.getString("key", ""))
         assertEquals("v0", deviceExp.getString("key", ""))
         assertEquals("v2", nonStickExp.getString("key", ""))
-
     }
 }
