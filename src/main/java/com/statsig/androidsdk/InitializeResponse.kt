fd59bbcbaefa5820908e28d79ba4918747c2e812
Kenny Yi <kenny@statsig.com>
2023-04-24 12:22:27 -0700

    fix indents

--- src/main/java/com/statsig/androidsdk/InitializeResponse.kt
+++ src/main/java/com/statsig/androidsdk/InitializeResponse.kt
@@ -12,43 +12,43 @@ enum class InitializeFailReason {
 
 sealed class InitializeResponse {
     data class FailedInitializeResponse(
-            @SerializedName("reason") val reason: InitializeFailReason,
-            @SerializedName("exception") val exception: Exception? = null,
-            @SerializedName("statusCode") val statusCode: Int? = null,
+        @SerializedName("reason") val reason: InitializeFailReason,
+        @SerializedName("exception") val exception: Exception? = null,
+        @SerializedName("statusCode") val statusCode: Int? = null,
     ): InitializeResponse()
     internal data class SuccessfulInitializeResponse(
-            @SerializedName("feature_gates") val featureGates: Map<String, APIFeatureGate>?,
-            @SerializedName("dynamic_configs") val configs: Map<String, APIDynamicConfig>?,
-            @SerializedName("layer_configs") var layerConfigs: Map<String, APIDynamicConfig>?,
-            @SerializedName("has_updates") val hasUpdates: Boolean,
-            @SerializedName("time") val time: Long,
+        @SerializedName("feature_gates") val featureGates: Map<String, APIFeatureGate>?,
+        @SerializedName("dynamic_configs") val configs: Map<String, APIDynamicConfig>?,
+        @SerializedName("layer_configs") var layerConfigs: Map<String, APIDynamicConfig>?,
+        @SerializedName("has_updates") val hasUpdates: Boolean,
+        @SerializedName("time") val time: Long,
     ): InitializeResponse()
 }
 
 internal data class APIFeatureGate(
-        @SerializedName("name") val name: String,
-        @SerializedName("value") val value: Boolean = false,
-        @SerializedName("rule_id") val ruleID: String = "",
-        @SerializedName("secondary_exposures") val secondaryExposures: Array<Map<String, String>> = arrayOf(),
+    @SerializedName("name") val name: String,
+    @SerializedName("value") val value: Boolean = false,
+    @SerializedName("rule_id") val ruleID: String = "",
+    @SerializedName("secondary_exposures") val secondaryExposures: Array<Map<String, String>> = arrayOf(),
 )
 
 internal data class APIDynamicConfig(
-        @SerializedName("name") val name: String,
-        @SerializedName("value") val value: Map<String, Any>,
-        @SerializedName("rule_id") val ruleID: String?,
-        @SerializedName("secondary_exposures") val secondaryExposures: Array<Map<String, String>> = arrayOf(),
-        @SerializedName("undelegated_secondary_exposures") val undelegatedSecondaryExposures: Array<Map<String, String>> = arrayOf(),
-        @SerializedName("is_device_based") val isDeviceBased: Boolean = false,
-        @SerializedName("is_user_in_experiment") val isUserInExperiment: Boolean = false,
-        @SerializedName("is_experiment_active") val isExperimentActive: Boolean = false,
-        @SerializedName("allocated_experiment_name") val allocatedExperimentName: String? = null,
-        @SerializedName("explicit_parameters") val explicitParameters: Array<String> = arrayOf()
+    @SerializedName("name") val name: String,
+    @SerializedName("value") val value: Map<String, Any>,
+    @SerializedName("rule_id") val ruleID: String?,
+    @SerializedName("secondary_exposures") val secondaryExposures: Array<Map<String, String>> = arrayOf(),
+    @SerializedName("undelegated_secondary_exposures") val undelegatedSecondaryExposures: Array<Map<String, String>> = arrayOf(),
+    @SerializedName("is_device_based") val isDeviceBased: Boolean = false,
+    @SerializedName("is_user_in_experiment") val isUserInExperiment: Boolean = false,
+    @SerializedName("is_experiment_active") val isExperimentActive: Boolean = false,
+    @SerializedName("allocated_experiment_name") val allocatedExperimentName: String? = null,
+    @SerializedName("explicit_parameters") val explicitParameters: Array<String> = arrayOf()
 )
 
 internal data class FeatureGate(
-        val name: String,
-        val details: EvaluationDetails,
-        val value: Boolean = false,
-        val ruleID: String = "",
-        val secondaryExposures: Array<Map<String, String>> = arrayOf()
+    val name: String,
+    val details: EvaluationDetails,
+    val value: Boolean = false,
+    val ruleID: String = "",
+    val secondaryExposures: Array<Map<String, String>> = arrayOf()
 )
