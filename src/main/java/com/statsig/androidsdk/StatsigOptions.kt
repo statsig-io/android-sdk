ec45bb78b2f6edebe7bef4e9d72dd920caca78d7
Xin Li <137219293+xinlili-statsig@users.noreply.github.com>
2023-10-12 16:47:56 -0700

    Add inline documents to statsig options (#159)

--- src/main/java/com/statsig/androidsdk/StatsigOptions.kt
+++ src/main/java/com/statsig/androidsdk/StatsigOptions.kt
@@ -20,14 +20,53 @@ private const val TIER_KEY: String = "tier"
  * callback to initialize will fire after, at most, the time specified
  */
 class StatsigOptions(
+    /**
+     The endpoint to use for all SDK network requests. You should not need to override this
+     (unless you have another API that implements the Statsig API endpoints)
+     */
     @SerializedName("api") var api: String = "https://api.statsig.com/v1",
+    /**
+     * By default, any custom event your application logs with Statsig.logEvent() includes the current
+     * root View Controller. This is so we can generate user journey funnels for your users. You can
+     * set this parameter to true to disable this behavior.
+     */
     @SerializedName("disableCurrentActivityLogging") var disableCurrentActivityLogging: Boolean = false,
+    /**
+     * Prevent the SDK from sending useful debug information to Statsig
+     */
     @SerializedName("disableDiagnosticsLogging") var disableDiagnosticsLogging: Boolean = false,
+    /**
+     * Used to decide how long the Statsig client waits for the initial network request to respond
+     * before calling the completion block. The Statsig client will return either cached values
+     * (if any) or default values if checkGate/getConfig/getExperiment is called before the initial
+     * network request completes
+     * if you always want to wait for the latest values fetched from Statsig server, you should set this to 0 so we do not timeout the network request.
+     */
     @SerializedName("initTimeoutMs") var initTimeoutMs: Long = 3000L,
+    /**
+     * By default, feature values for a user are fetched once during Statsig.start and don't change
+     * throughout the session. Setting this value to true will make Statsig periodically fetch updated
+     * values for the current user.
+     */
     @SerializedName("enableAutoValueUpdate") var enableAutoValueUpdate: Boolean = false,
+    /**
+     * overrides the stableID in the SDK that is set for the user
+     */
     @SerializedName("overrideStableID") var overrideStableID: String? = null,
+    /**
+     * Whether or not the SDK should block on loading saved values from disk. By default, we block on
+     * loading from disk, so we guarantee cache will be loaded when sdk is being used.
+     */
     @SerializedName("loadCacheAsync") var loadCacheAsync: Boolean = false,
+    /**
+     * Provide a Dictionary representing the "initiailize response" required  to synchronously initialize the SDK.
+     * This value can be obtained from a Statsig server SDK.
+     */
     @SerializedName("initializeValues") var initializeValues: Map<String, Any>? = null,
+    /**
+     * When disabled, the SDK will not hash gate/config/experiment names, instead they will be readable as plain text.
+     * Note: This requires special authorization from Statsig
+     */
     @SerializedName("disableHashing") var disableHashing: Boolean? = false,
 ) {
 
